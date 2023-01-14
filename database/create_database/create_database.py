291a733d60e17b59a3657bad57c44a4874975944
isi <isidora.iyho@gmail.com>
2023-01-14 18:32:47 -0300

    docker2.0

--- database/create_database/create_database.py
+++ database/create_database/create_database.py
@@ -2,41 +2,72 @@ import mysql.connector
 import os, time
 
 def create_database(db_connection,db_name,cursor):
-	cursor.execute(f"CREATE DATABASE {db_name};")
-	cursor.execute(f"COMMIT;")
-	cursor.execute(f"USE {db_name};")
-	
-	cursor.execute('''CREATE TABLE Programa (
+    cursor.execute(f"COMMIT;")
+    cursor.execute(f"CREATE DATABASE {db_name};")
+    cursor.execute(f"COMMIT;")
+    cursor.execute(f"USE {db_name};")
+    cursor.execute(f"COMMIT;")
+    
+    cursor.execute('''CREATE TABLE Programa (
         id INT NOT NULL AUTO_INCREMENT,
         nombre varchar(255) NOT NULL,
         director varchar(255),
         correo varchar(255),
         PRIMARY KEY (id)
         );''')
-	cursor.execute("COMMIT;") 
+    cursor.execute("COMMIT;") 
 
-    # cursor.execute(f"DROP DATABASE IF EXISTS {db_name};")
-    # cursor.execute(f"CREATE DATABASE {db_name};")
-	# cursor.execute(f"COMMIT;")
-	# cursor.execute(f"USE {db_name};")
-	
-	# cursor.execute('''CREATE TABLE Programa (
-    #     id INT NOT NULL AUTO_INCREMENT,
-    #     nombre varchar(255) NOT NULL,
-    #     director varchar(255),
-    #     correo varchar(255),
-    #     PRIMARY KEY (id)
-    #     );''')
-    # cursor.execute("COMMIT;") 
+    cursor.execute('''CREATE TABLE Estudiante (
+        rut varchar(15) NOT NULL PRIMARY KEY, 
+        nombre varchar(255) NOT NULL,
+        correo varchar(255),
+        id_programa INT NOT NULL,
+        FOREIGN KEY (id_programa) REFERENCES Programa (id)
+        );''')
+    cursor.execute("COMMIT;") 
 
-    # cursor.execute('''CREATE TABLE Estudiante (
-    #     rut varchar(15) NOT NULL PRIMARY KEY, 
-    #     nombre varchar(255) NOT NULL,
-    #     correo varchar(255),
-    #     id_programa INT NOT NULL,
-    #     FOREIGN KEY (id_programa) REFERENCES Programa (id)
-    #     );''')
-    # cursor.execute("COMMIT;") 
+    cursor.execute('''CREATE TABLE Tener_arancel (
+        id INT NOT NULL AUTO_INCREMENT,
+        año int NOT NULL,
+        valor int NOT NULL,
+        id_programa INT NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (id_programa) REFERENCES Programa (id)
+        );''')
+    cursor.execute("COMMIT;") 
+
+    cursor.execute('''CREATE TABLE Becas (
+        id INT NOT NULL AUTO_INCREMENT,
+        nombre varchar(255),
+        porcentaje INT NOT NULL,
+        año INT NOT NULL,
+        semestre INT NOT NULL,
+        rut_estudiante varchar(15) NOT NULL, 
+        PRIMARY KEY (id),
+        FOREIGN KEY (rut_estudiante) REFERENCES Estudiante (rut)
+        );''')
+    cursor.execute("COMMIT;") 
+
+    cursor.execute('''CREATE TABLE Comprobante (
+        num_boleta INT NOT NULL PRIMARY KEY, 
+        monto INT NOT NULL,
+        fecha_pago DATE,
+        arancel_matricula VARCHAR(32),
+        enlace_foto VARCHAR(255),
+        rut_estudiante varchar(15) NOT NULL, 
+        FOREIGN KEY (rut_estudiante) REFERENCES Estudiante (rut)
+        );''')
+    cursor.execute("COMMIT;") 
+
+    cursor.execute('''CREATE TABLE Cuenta (
+        id INT NOT NULL AUTO_INCREMENT,
+        nombre varchar(255) NOT NULL,
+        correo varchar(255),
+        contraseña varchar(255) NOT NULL,
+        tipo varchar(255) NOT NULL,
+        PRIMARY KEY (id)
+        );''')
+    cursor.execute("COMMIT;") 
 
 def insert_data(cursor):
     print("insert")
@@ -66,7 +97,7 @@ not_connected = True
 while(not_connected):
 	try:
 		print(DATABASE_IP,"IP")
-		db_connection = mysql.connector.connect(user=DATABASE_USER,host=DATABASE_IP,password=DATABASE_USER_PASSWORD)
+		db_connection = mysql.connector.connect(user=DATABASE_USER,host=DATABASE_IP,password=DATABASE_USER_PASSWORD) #auth_plugin='mysql_native_password'
 		not_connected = False
 
 	except Exception as e:
@@ -77,8 +108,8 @@ while(not_connected):
 cursor = db_connection.cursor()
 
 try:
-	cursor.execute(f"USE {DATABASE}")
-	print(f"Database: {DATABASE} already exists")
+    cursor.execute(f"USE {DATABASE}")
+    print(f"Database: {DATABASE} already exists")
 except Exception as e:
     create_database(db_connection,DATABASE,cursor)
     # insert_data(cursor)
